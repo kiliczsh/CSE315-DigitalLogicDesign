50badb89d2f6c54e6cc94b9e5585bc5504062562
Muhammed Kılıç <muhammeddkilicc@gmail.com>
2017-12-17 15:03:11 +0300

    asdasd

--- Step-3/20BitComparator.v
+++
@@ -1,5 +0,0 @@
-module BitComparator20(input [19:0]a,input [19:0]b,output eq,output gt,output lt);
-    assign eq = a == b;
-    assign gt = a > b;
-    assign lt = a < b;
-endmodule
--- Step-3/ControlUnit.v
+++
@@ -1,1 +0,0 @@
-// Control Unit Verilog
